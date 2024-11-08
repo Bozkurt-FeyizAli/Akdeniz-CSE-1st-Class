dda3e6d2c350b391c92fa11b0f91084775d96b8a
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-08 22:53:21 +0300

    Update Assignment4_20220808073.java

--- aralık 23/assignment4_20220808073/Assignment4_20220808073.java
+++ aralık 23/assignment4_20220808073/Assignment4_20220808073.java
@@ -24,3 +24,56 @@
         double[] studentScores= new double[countCategory(studentInformation)];
         // get information
         getCategoryInformatinsAsArray(categoryInformation, category,
+    public static  String gradeLetter(double grade ){
+        if      (grade>=87.5) return "AA";
+        else if (grade>=80.5) return "BA";
+        else if (grade>=73.5) return "BB";
+        else if (grade>=66.5) return "CB";
+        else if (grade>=59.5) return "CC";
+        else if (grade>=52.5) return "DC";
+        else if (grade>=45.5) return "DD";
+        else if (grade>=34.5) return "FD";
+        else                  return "FF";
+    }
+
+    public static double gpaPoints(double grade){
+        double result;
+            if      (grade>=87.5) result = 4.0;
+            else if (grade>=80.5) result = 3.5;
+            else if (grade>=73.5) result = 3.0;
+            else if (grade>=66.5) result = 2.5;
+            else if (grade>=59.5) result = 2.0;
+            else if (grade>=52.5) result = 1.5;
+            else if (grade>=45.5) result = 1.0;
+            else if (grade>=34.5) result = 0.5;
+            else                  result = 0.0;
+        return result;
+    }
+
+    public static String status(double grade){
+        String result;
+            if      (grade>=59.5) result = "passed";
+            else if (grade>=45.5) result = " conditionally passed";
+            else                  result = "failed";
+        return result;}
+        
+    public static String formatCategoryName(String name1){
+        char upperCase1= name1.charAt(0);
+        upperCase1= Character.toUpperCase(upperCase1);
+        name1 =name1.substring(1);
+        name1= name1.toLowerCase();
+        name1= upperCase1+name1;
+        return name1;
+    }
+    
+    public static Boolean isQuantityValid(int quantity){
+        Boolean result =  (quantity>0) ? true : false;
+        return result;
+    } 
+    
+    public static Boolean isWeightValid(int weight, int sumweight){
+        Boolean result = (weight>=0&&sumweight+weight<=100)? true :false;
+        return result;
+    }
+    
+}
