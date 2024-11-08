b633c79f33005335d4f0df9adac03b8d428f96ba
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-08 22:54:44 +0300

    Update Assignment4_20220808073.java

--- aralık 23/assignment4_20220808073/Assignment4_20220808073.java
+++ aralık 23/assignment4_20220808073/Assignment4_20220808073.java
@@ -24,6 +24,135 @@
         double[] studentScores= new double[countCategory(studentInformation)];
         // get information
         getCategoryInformatinsAsArray(categoryInformation, category,
+    public static void studentGrades(String studentScoresFile,
+                                     String[] category, int[] quantity,
+                                     int[] weight, String writeErrors,
+                                     double[] studentGrades,
+                                     String[] studentNames) {
+ 
+        File file = new File(studentScoresFile);
+        try (Scanner input= new Scanner(file);){
+        for (int index = 0;index<countCategory(studentScoresFile); index++) {
+            boolean studentGradeVaild=true;
+            double temporaryGrade=0;
+            double score=0;
+            studentNames[index]= input.next();
+            for(int j=0;category.length; j++){
+                double categoryGrade = 0;
+                for(int k=0; k<quantity[j] ;k++){
+                        score = Double.parseDouble(input.next()) / quantity[j];
+                        categoryGrade += score;
+                    if(score<0||score>100){
+                        studentGradeVaild=false;
+                    }
+                    // if(quantity[j]-1==k){
+                    //     break;
+                    //}
+                }
+                temporaryGrade += categoryGrade * weight[j]/100.0;
+                if(!studentGradeVaild){
+                    studentGrades[index]=Double.MIN_VALUE;
+                }
+                else{
+                    studentGrades[index]=temporaryGrade;
+                }
+                // if(j==category.length-1){
+                //     break;
+                //}
+            }
+        }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        bindeYuzeKadar(studentGrades);
+    }
+
+    public static File errorStudentGradeWriter(String studentScoresFile,
+                                               String[] studentName,
+                                               double[] studentScores){
+
+        File file= new File(studentScoresFile);
+        try {
+            file.createNewFile();
+            FileWriter informationWriter = new FileWriter(file);
+            for(int i=0; i<studentName.length;i++){
+                if(studentScores[i]==Double.MIN_VALUE){
+                informationWriter.write
+("ERROR: Student "+studentName[i]+
+ " - cannot calculate due to invalid grade.\n");
+                }
+                else{continue;}
+            }
+            informationWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: Belirtilen dosya bulunamadı.");
+            System.out.println("ERROR: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("ERROR: IOException occurred");
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return file;
+    }
+
+    public static void bindeYuzeKadar(double[] array){
+        for(int i=0;i<array.length;i++){
+            if(array[i]!=Double.MIN_VALUE)
+            array[i]=(double)((int)(array[i]*100))/100.0;
+        }
+    }
+
+    public static int countCategory(String studentScoresFile)  {
+        int numberOfCategories=0;
+        File file = new File(studentScoresFile);
+        if(!file.exists()){
+            System.out.printf("ERROR: %s file does not exist!",
+                                      studentScoresFile);
+            System.out.println();
+        }
+        else{
+            try (Scanner input = new Scanner(new File(studentScoresFile)); ){
+                while (input.hasNext()) {
+                    String line = input.nextLine();
+                    numberOfCategories++;
+                } 
+                input.close();
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("ERROR: Belirtilen dosya bulunamadı.");
+            }
+        }
+        return numberOfCategories;
+    }
+
+    public static void getCategory(String[] category,
+                                      int[] quantity,
+                                      int[] weight,
+                                      String studentScoresFile){
+
+        File file = new File(studentScoresFile);
+        if(!file.exists()){
+        System.out.printf("ERROR: File %s does not exist!",
+                                  studentScoresFile);
+        }
+        else{
+            try (Scanner input = new Scanner(file); ) 
+            {   
+                int index=0;
+                while (input.hasNext()) 
+                {
+                    category[index] = input.next();
+                    quantity[index]= input.nextInt();
+                    weight[index]= input.nextInt();
+                    index++;
+                } 
+                input.close();
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("ERROR: Belirtilen dosya bulunamadı.");
+            }
+        }
+    }
+
     public static void writeGrades(String[] student, double[] grade,
                                    String studentGradeFileName,
                                    String errorLogFileName){
@@ -64,6 +193,36 @@
 
     public static void formatCategoryName(String name[]){
             for(int i=0;i<name.length;i++){
+               name[i]=formatCategoryName(name[i]);
+            }
+    }
+    
+    public static Boolean isQuantityValid(int quantity[]){
+        Boolean result =true;
+            for(int i=0;i<quantity.length;i++){
+                result=isQuantityValid(quantity[i]);
+                if(result==false)
+                break;
+            }
+        return result;
+    } 
+    
+    public static Boolean isWeightValid(int weight[]){
+        Boolean result=true;
+        int sumweight=0;
+        for(int i=0;i<weight.length;i++){
+           if(weight[i]<0||weight[i]>100){
+                result=false;
+                break;
+            }            
+            sumweight+=weight[i];
+        }
+        if(sumweight!=100){
+            result=false;
+        }
+        return result;
+    }
+    
     public static  String gradeLetter(double grade ){
         if      (grade>=87.5) return "AA";
         else if (grade>=80.5) return "BA";
@@ -117,3 +276,15 @@
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
