28ca6e4da2f8c307abf228efec5d2ae0948459d5
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-12 21:35:45 +0300

    Update Assignment04_20220808073.java

--- Assignmetns 23-24/bahar 24/Assignment_04/Assignment04_20220808073.java
+++ Assignmetns 23-24/bahar 24/Assignment_04/Assignment04_20220808073.java
@@ -1,3 +1,30 @@
+class InvalidRankException extends RuntimeException{
+    int rank;
+    InvalidRankException(int rank){
+        this.rank=rank;
+    }
+    @Override
+    public String toString(){
+        return "InvalidRankException: " + rank ;
+    }
+}
+
+class DepartmentMismatchException extends RuntimeException{
+    Department department;
+    Teacher person;
+    Course course;
+    Teacher chair;
+    DepartmentMismatchException(Teacher person, Course course){
+        this.department=null;
+        this.person=person;
+        this.course=course;
+    }
+    DepartmentMismatchException(Department department, Teacher person)
+                                          throws GeneralErrorException{
+        this.course=null;
+        this.department=department;
+        this.person=person;
+    }
     public String toString(){
         if(course==null)
             return "DepartmentMismatchException: " + person.getName() +
