a39a706314827f95bc906f7c4175e174793af7ac
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-12-30 20:21:03 +0300

    Update Quiz2_20220808073.java

--- ocak 24/quiz-2/Quiz2_20220808073.java
+++ ocak 24/quiz-2/Quiz2_20220808073.java
@@ -123,3 +123,58 @@ public class Quiz2_20220808073{
         return true; 
     }
 
+    //     public static boolean isMarkovMatrix(double[][] array){
+    //     for(int i=0;i<array.length;i++){
+    //         for(int j=0;j<array.length;j++){
+    //             if(array[i][j]<0)
+    //             return false;
+    //         }
+    //     }
+    //     double colum=0;
+    //     for(int i=0;i<array.length;i++){
+    //         for(int j=0;j<array.length;j++){
+    //              colum+=array[j][i];
+    //              if(colum!=1)
+    //              return false;
+        
+    //         }
+    //         colum=0;
+    //     }
+    //     return true; 
+    // }
+    // done
+    public static File movingAverage(String sourceFileName, String writeFileName, int windowSize) throws Exception{
+        File file= new File(writeFileName);
+        int indexSouce=0;
+        File readen= new File(sourceFileName);
+        Scanner read= new Scanner(readen);
+        while(read.hasNext()){
+           String a= read.next();
+           indexSouce++;
+        }
+        read.close();
+        File Areaden= new File(sourceFileName);
+        double[] datas= new double[indexSouce];
+        Scanner readSource= new Scanner(Areaden);
+        int indexOfArrayS=0;
+        while(readSource.hasNext()){
+            datas[indexOfArrayS]=Double.parseDouble(readSource.next());
+        }
+        readSource.close();
+        double[] result= new double[windowSize];
+        for(int i=0;i<windowSize;i++){
+            double sumAverage=0;
+            for(int j=i;j<windowSize;j++){
+                sumAverage+=datas[j];
+            }
+            sumAverage/=windowSize;
+            result[i]= sumAverage;
+        }
+        PrintWriter writer= new PrintWriter(file);
+        for(int i=0;i<windowSize;i++){
+            writer.print((double)((int)(result[i]*10))/10+",");
+        }
+        writer.close();
+        return file; 
+    }
+}
