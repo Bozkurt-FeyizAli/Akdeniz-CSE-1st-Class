d5d69bec295a2cc6dfdee01742468c625a27d3bd
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-12-30 20:20:59 +0300

    Update Quiz2_20220808073.java

--- ocak 24/quiz-2/Quiz2_20220808073.java
+++ ocak 24/quiz-2/Quiz2_20220808073.java
@@ -60,3 +60,66 @@ public class Quiz2_20220808073{
         }
         return result;
     }
+
+    public static void swap(int[] array, int a, int b){
+        int swap=a;
+        array[a]=b;
+        array[b]=swap;
+    }
+
+    public static int  factorial(int n){
+        if(n==1){
+            return 1;
+        }
+        else return n* factorial(n-1);
+    }
+    // done
+    public static boolean isPalindrome(String word){
+        int start=0;
+        int end=word.length();
+        if(word.length()==1||word.equals("")){
+            return true;
+        }
+        else if(word.charAt(start)!=word.charAt(end-1)){
+            return false;
+        }
+        else{
+            return isPalindrome(word.substring(start+1, end-1));
+        }
+    }
+    //done
+    public static int[] decimalToBinary(int n){
+        int index=0;
+        int number=n;
+        for(int i=0;n!=0;i++){
+            n/=2;
+            index++;
+        }
+        int result[] = new int[index];
+        for(int i=1;number!=0;i++){
+            result[result.length-i]=number%2;
+            number/=2;
+        }
+        return result;
+    }
+      //Done
+    public static boolean isMarkovMatrix(int[][] array){
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array.length;j++){
+                if(array[i][j]<0)
+                return false;
+            }
+        }
+        double colum=0;
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array.length;j++){
+                 colum+=array[j][i];
+                 if(colum!=1)
+                 return false;
+        
+            }
+            colum=0;
+        }
+        return true; 
+    }
+
