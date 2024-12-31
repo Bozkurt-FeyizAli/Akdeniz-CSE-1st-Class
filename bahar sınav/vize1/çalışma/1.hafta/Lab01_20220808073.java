ca7cf248db0d257b13af004c93c1cd55675a85c6
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-12-31 10:11:53 +0300

    Update Lab01_20220808073.java

--- bahar sınav/vize1/çalışma/1.hafta/Lab01_20220808073.java
+++ bahar sınav/vize1/çalışma/1.hafta/Lab01_20220808073.java
@@ -60,3 +60,106 @@ class Circle1{
     public Date getDateOfCreation() {
         return dateOfCreation;
     }
+
+
+    // costructor is a special kind of method not doing anything
+    // overrid is used for give a special output for sth
+    @Override
+    public String toString(){
+        return String.format("Circle with %s colour, with %d area", colour, radius);
+    }
+    
+    static int count=0;
+    // ststic used for when we need to use a value later
+     
+    static void display(Circle1[] circles){
+        for (Circle1 c : circles) {
+            if(!c.equals(null))
+            System.out.println(c);
+        }
+    }
+    // mutator
+    // immutable = un changeable
+    void setRadius(int radius){
+        if(radius>=0)
+        this.radius= radius;
+        else
+        System.out.println("ERROR: invalid values ");
+    }
+
+}
+
+class Color{
+    String name="RED";
+        @Override
+    public String toString(){
+        return String.format("Circle with %s colour, with %d m^2 area", name, 25);
+    }
+
+    class Fields{
+        static int count=0;
+        static Fields[] employee= new Fields[8]; 
+        String name="Default Employee";
+        double salary=23277.53;
+        int id=-1;
+        int level=0;
+
+        Fields(String name, double salary, int level){
+            this.name= Character.toUpperCase(name.charAt(0))+name.substring(1).toLowerCase();
+            this.salary= salary;
+            this.level= level;
+
+        }
+        int random(int level){
+            if(level==2)
+            return 1000+(int)(Math.random()*9+1)*100+(int)(Math.random()*9+1)*10+(int)(Math.random()*9+1);
+            else return 2143;
+        }
+        public int getID(){
+        System.out.println("ID data is accesed.");
+        return id;
+        }
+        public String getName(){
+        System.out.println("name data is accesed.");
+        return name;
+        }
+        public double getSalary(){
+        System.out.println("salary data is accesed.");
+        return salary;
+        }
+        public int getLevel(){
+        System.out.println("name level is accesed.");
+        return level;
+        }
+        void setName(String name){
+        this.name= name;
+        }
+        void setSalary(double salary){
+            if(salary>=0)
+        this.salary= salary;
+        else
+        System.out.println("ERROR: invalid value");
+        }
+        int generateId(int level){
+            return this.level*1000+(int)(Math.random()*9+1)*100+(int)(Math.random()*9+1)*10+(int)(Math.random()*9+1);
+        }
+        Fields[] promote(Fields[] employee){
+            return employee;
+        }
+        @Override
+        public String toString(){
+            return String.format("name: ", name);
+        }
+
+        
+    }
+}
+
+
+
+
+
+
+
+
+    
