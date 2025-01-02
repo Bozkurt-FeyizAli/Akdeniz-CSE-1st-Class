a1f6fb2f53fc2304c31cd13c94607be5e95dc14b
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-02 17:29:11 +0300

    Update Lab05_20220808073.java

--- bahar sınav/vize1/çalışma/5.hafta/Lab05_20220808073.java
+++ bahar sınav/vize1/çalışma/5.hafta/Lab05_20220808073.java
@@ -95,3 +95,126 @@ class CPU{
 
 
 }
+
+class RAM{
+    String type;
+    int capacity;
+    int[][] memory;
+    RAM(String type, int capacity){
+        setCapacity(capacity);
+        setType(type);
+        initMemory();
+    }
+    public void setValue(int i, int j, int result) {
+        if(check(i, j))
+        memory[i][j]=result;
+        else
+        throw new UnsupportedOperationException("Unimplemented method 'setValue'");
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public void setMemory(int[][] memory) {
+        this.memory = memory;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public int[][] getMemory() {
+        return memory;
+    }
+    public String getType() {
+        return type;
+    }
+    // use random class
+    private void initMemory(){
+        Random random = new Random();
+       this.memory= new int[capacity][capacity];
+       for(int i=0;i<memory.length;i++){
+           for(int j=0;j<memory.length;j++){
+            memory[i][j]=random.nextInt(0,10);
+           }
+       }
+    }
+    public int random(){
+        return (int) Math.random()*10+1;
+    }
+    private boolean check(int i, int j) throws ArithmeticException{
+     // add
+     CPU a= new CPU(type, j);
+        if(j<capacity+1&&j>-1||capacity+1>i&&i>-1)
+        return true;
+        else 
+        throw 
+        new ArithmeticException();
+        // // computationException(cpu, i, j);
+        
+        
+    }
+    public int getValue(int i, int j) throws computationException{
+        if(check(i, j))
+        return memory[i][j];
+        else return -1;
+    }
+    // public void setValue(int i, int j, int value)throws ArrayIndexOutOfBoundsException{
+    //     if(value<11&&value>-1)
+    //     if(check(i, j))
+    //     this.memory[i][j]=value;
+    //     else
+    //     throw new AssertionError(i);
+        
+    //      System.out.println("ERROR");
+    
+
+    // }
+    public String toString(){
+        return  "RAM: " + type + ", " + capacity + "GB";
+    }
+
+
+
+}
+
+// out of bound memory
+class Laptop extends Computer{
+    int milliAmp;
+    int battery;
+    Laptop(CPU cpu, RAM ram, int milliAmp){
+        super(cpu, ram);
+        setMilliAmp(milliAmp);
+        this.battery=30;
+
+    }
+    public void setBattery(int battery) {
+        this.battery = battery;
+    }
+    public void setMilliAmp(int milliAmp) {
+        this.milliAmp = milliAmp;
+    }
+    public int getBattery() {
+        return battery;
+    }
+    public int getMilliAmp() {
+        return milliAmp;
+    }
+    public int  batteryPercentage(){
+        return getBattery();
+    }
+    public void charge(){
+        if(battery<90){
+            this.battery+=2;
+        }
+    }
+    @Override
+    public void run(){
+        if(battery>5)
+        this.battery-=5;
+    }
+    public String toString(){
+        return super.toString()+" "+Integer.toString(battery);
+    }
+}
+
