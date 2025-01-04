eac555aff59c10ce8b28fcd52e3ba82caa294199
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-04 09:48:10 +0300

    Update vize1.java

--- bahar sınav/vize1/çalışma/vize1.java
+++ bahar sınav/vize1/çalışma/vize1.java
@@ -50,3 +50,130 @@ abstract class Shape{
     abstract double volume();
 
 }
+
+class Sphere extends Shape implements Resizable{
+    private double radius;
+    private double area;
+    private double perimeter;
+    private double volume;
+    private String color;
+    Sphere(int radius, String color){
+        setRadius(radius);
+    }
+        
+    @Override
+    public void resize(double facor) {
+        radius*=facor;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setRadius(double radius) throws InvalidRadiusException {
+        if(radius<0)
+            throw new InvalidRadiusException("color");
+        this.radius = radius;
+    }
+    public double getArea() {
+        return area;
+    }
+    public String getColor() {
+        return color;
+    }
+    public double getPerimeter() {
+        return perimeter;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public double getVolume() {
+        return volume;
+    }
+    
+    @Override
+    public double volume(){
+        return (4/3)*Math.PI*radius*radius*radius;
+    }
+    @Override
+    public double perimeter(){
+        return 4*Math.PI*radius*radius;
+    }
+    @Override 
+    public double area(){
+        return perimeter();
+    }
+
+}
+
+class Rectangle extends Shape implements Scalable{
+    private double side1;
+    private double side2;
+    private double side3;
+    private double area;
+    private double perimeter;
+    private double volume;
+    private String color;
+    Rectangle(int side1, int side2, int side3, String color){
+        setColor(color);  
+            setSide1(side1);
+            setSide2(side2);
+            setSide3(side3);
+
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setSide1(double side1) throws InvalidLengthException {
+        if(side1<0)
+            throw new InvalidLengthException("color");
+        this.side1 = side1;
+    }
+    public void setSide2(double side2) throws InvalidLengthException{
+        if(side2<0)
+            throw new InvalidLengthException("color");
+        this.side2 = side2;
+    }
+    public void setSide3(double side3) throws InvalidLengthException{
+        if(side3<0)
+            throw new InvalidLengthException("color");
+        this.side3 = side3;
+    }
+    public double getArea() {
+        return area;
+    }
+    public String getColor() {
+        return color;
+    }
+    public double getPerimeter() {
+        return perimeter;
+    }
+    public double getSide1() {
+        return side1;
+    }
+    @Override
+    public void scale(double factor) {
+        side1*=factor;
+        side2*=factor;
+        side3*=factor;
+    }
+    public double getSide2() {
+        return side2;
+    }
+    public double getSide3() {
+        return side3;
+    }
+    public double getVolume() {
+        return volume;
+    }
+    @Override
+    public double volume(){
+        return side1*side2*side3;
+    }
+    @Override
+    public double perimeter(){
+        return 2*side1*side2+2*side1*side3+2*side2*side3;
+    }
+    @Override 
+    public double area(){
+        return perimeter();
+    }
+}
