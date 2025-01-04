ffb34e89daf735d3af720713b05f856fd302d4e8
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-04 09:46:37 +0300

    Update Lab07_20220808073.java

--- bahar sınav/vize1/çalışma/7. hafta/Lab07_20220808073.java
+++ bahar sınav/vize1/çalışma/7. hafta/Lab07_20220808073.java
@@ -48,3 +48,63 @@ class Account{
     }
 }
 
+class Customer{
+    private String name;
+    private ArrayList<Account> accounts;
+    Customer(String name){
+        this.name=name;
+        accounts= new ArrayList<>();
+    }
+    public Account getAccount(String accountNumber) throws AccountNotFoundException{
+        for (Account account : accounts) {
+            if(account.getAccountNumber().equals(accountNumber))
+                return account;
+        }
+        throw new AccountNotFoundException(this, accountNumber);
+    }
+    public String getName() {
+        return name;
+    }
+    public void addAccount(Account account)throws AccountAlreadyExistException{
+        try {
+            getAccount(account.getAccountNumber());
+            throw new AccountAlreadyExistException(this, account);
+        } catch (AccountNotFoundException e) {
+            accounts.add(account);
+        }
+        finally{
+            System.out.println(this);
+        }
+
+        System.out.println("Added account: "+account);
+    }
+
+    public void removeAccount(Account account) throws AccountNotFoundException{
+            getAccount(account.getAccountNumber());
+            accounts.remove(account);
+    }
+    public void transfer(String fromAccount, Account toAccount, double amount)throws InvalidTransactionException{
+       try {
+            Account from=getAccount(toAccount.getAccountNumber());
+            from.withdraw(amount);
+            toAccount.deposit(amount);
+       } catch (AccountNotFoundException e) {
+            System.out.println(e.getMessage());
+       }catch(InsufficientFundsException e){
+            System.out.println(e.getMessage());
+       }catch(InvalidTransactionException e){
+            throw new InvalidTransactionException(e, "Can not transfer funds from "+fromAccount+"to account "+toAccount.getAccountNumber());
+       }
+       
+    }
+
+    @Override
+    public String toString() {
+        String message= this.getName()+":\n\t";
+        for (Account account : accounts) {
+            message+=account+"\n\t";
+        }
+        return message;
+    }
+    
+}
