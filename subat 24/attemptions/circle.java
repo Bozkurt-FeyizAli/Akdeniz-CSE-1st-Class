f4e93c96b4dc3614ab7ffe940f7613a230b6a768
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-11 21:24:00 +0300

    11,01,25

---
+++ subat 24/attemptions/circle.java
@@ -0,0 +1,17 @@
+// public class circle {
+//     public static void main(String[] args){
+//         abc ss= new abc();
+
+//         System.out.println(ss.getArea(5.0));
+//     }
+
+
+
+
+
+    
+//}
+ 
+
+
+    
