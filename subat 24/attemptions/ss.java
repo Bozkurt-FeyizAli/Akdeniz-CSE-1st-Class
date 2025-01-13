945528b206e5979c9e000b3d9be6c4b356c61a56
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-13 17:56:05 +0300

    Create ss.java

---
+++ subat 24/attemptions/ss.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+
+class abc{
+
+    public static void main(String[] args) {
+        
+    System.out.println((72-58.8)/23.6735);
+  
+// Statistic s= new Statistic();
+
+// ArrayList<Integer> results= new ArrayList<>();
+// File f= new File("ErasmusResults.txt");
+
+// try( Scanner input = new Scanner(f);){
+//   for(int i=0; input.hasNext(); i++){
+//     String bir= input.next();
+//     bir=input.next();
+//     bir=input.next();
+//     results.add(i, Integer.parseInt(input.next()));
+//   }
+// }
+// catch(Exception e){
+//     System.out.println(e.getMessage());
+// }
+// double[] Eresults = new double[results.size()];
+// for(int i=0;i<results.size();i++){
+//     Eresults[i]=results.get(i);
+// }
+
+// System.out.println(s.mean(Eresults));
+// System.out.println(Math.sqrt(s.varianceS_2(Eresults)));
+
+
+  
+
+
+
+ 
+
+    }
+
+
+    
+
+
+
+
+
+
+}
