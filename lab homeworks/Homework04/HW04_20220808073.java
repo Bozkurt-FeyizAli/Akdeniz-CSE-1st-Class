8060959c58d48c8eb90604467155b3c421f82a6d
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-19 18:43:23 +0300

    Create HW04_20220808073.java

---
+++ lab homeworks/Homework04/HW04_20220808073.java
@@ -0,0 +1,49 @@
+public class HW04_20220808073{
+    public static void main(String[] args){
+    //     CPU cpu= new CPU("i5_13500K", 90000000);
+    //     RAM ram = new RAM("DDR5 ", 10000);
+    //     Computer c = new Computer(cpu, ram);
+    //     try{
+    //     c.run();
+    //     String[] s= { "a", "s", "d" ,"f"};
+    //     Desktop d= new Desktop(cpu, ram, s);
+    //     System.out.println(d);
+    //     System.out.println(double.class);
+    //     }
+    //     catch(ComputationException e){
+    //         System.out.println(e);
+    //     }
+        
+
+    //  cpu = new CPU("Intel", 6000);
+    //      ram = new RAM("DDR4", 128);
+
+    //     // Laptop oluştur
+    //     Laptop laptop = new Laptop(cpu, ram, 100000);
+
+    //     // Desktop oluştur
+    //     String[] peripherals = {"Keyboard", "Mouse", "Monitor"};
+    //     Desktop desktop = new Desktop(cpu, ram, peripherals);
+
+    //     // Bilgisayarları çalıştır
+    //     laptop.run();
+    //     desktop.run();
+
+    //     // Laptop'un şarj durumunu kontrol et
+    //     System.out.println("Laptop Battery Percentage: " + laptop.batteryPercentage());
+
+    //     // Laptop'u şarj et
+    //     laptop.charge();
+
+    //     // Laptop'u tekrar çalıştır
+    //     laptop.run();
+
+    //     // Laptop'un şarj durumunu kontrol et
+    //     System.out.println("Laptop Battery Percentage After Charging: " + laptop.batteryPercentage());
+
+    //     // Bilgisayarları yazdır
+    //     System.out.println("Laptop: " + laptop);
+    //     System.out.println("Desktop: " + desktop);
+
+    }
+}
