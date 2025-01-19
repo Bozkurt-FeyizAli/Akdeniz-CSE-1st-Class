adfbfb60b792da3270b5460eb7cb19e43dac789f
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-19 18:43:34 +0300

    Update HW04_20220808073.java

--- lab homeworks/Homework04/HW04_20220808073.java
+++ lab homeworks/Homework04/HW04_20220808073.java
@@ -47,3 +47,66 @@ public class HW04_20220808073{
 
     }
 }
+class Computer{
+    protected CPU cpu;
+    protected RAM ram;
+    
+    Computer(CPU cpu, RAM ram){
+         setCpu(cpu);
+         setRam(ram);
+    }
+
+    public void setCpu(CPU cpu) {
+        this.cpu = cpu;
+    }
+    public void setRam(RAM ram) {
+        this.ram = ram;
+    }
+    public CPU getCpu() {
+        return cpu;
+    }
+    public RAM getRam() {
+        return ram;
+    }
+    public void run() throws ComputationException, MemoryException{
+        int result = 0;
+        int index=0;
+        try {
+            for (index = 0; index < ram.getCapacity(); index++) 
+                result=cpu.compute(result, ram.getValue(index, index));
+        } catch (MemoryException e) {
+            throw new MemoryException(ram, 0, 0);
+        } catch (InterruptedException e) {
+            e.getStackTrace();
+        }
+        catch(ComputationException e){
+            try {
+                result =
+                e.fixComputation(result, ram.getValue(result, 
+                                         ram.getValue(index, index)));
+                ram.setValue(0, 0, result);
+            } catch (Exception ex) {
+                e.getStackTrace();
+            }
+        }
+        ram.setValue(0, 0, result);
+    }
+    public int[] sumDiagonal(int[][] array) {
+        int[] sum = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            sum[i] += array[i][i];
+        }
+        return sum;
+    }
+    public int sumDigonal(int[][] array) throws ComputationException{
+        int result=0;
+        for(int i=0;i<array.length;i++){
+            result+=ram.getValue(i, i);
+        }
+        return result;
+    }
+    @Override
+        public String toString(){
+             return "Computer: "+cpu+" "+ram;
+        }
+}
